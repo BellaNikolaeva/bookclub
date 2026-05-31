9c97d03e081c38115f70ba521aa6b90444b71ec1
bella <b.nikolaeva@munikorn.com>
2026-05-31 15:38:01 +0300

    Fix duplicate path declaration

--- server.js
+++ server.js
@@ -20,7 +20,6 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-const path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 
 // ---------- API endpoints ----------
